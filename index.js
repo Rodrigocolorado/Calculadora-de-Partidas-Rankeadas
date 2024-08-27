411f8fb4092a8d07a5a46018afe653e516a58a2f
Rodrigo RJ <151276298+Rodrigocolorado@users.noreply.github.com>
2024-08-27 00:13:45 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+function calcularRank(vitorias, derrotas) {
+  const saldo = vitorias - derrotas;
+  let nivel;
+
+  if (vitorias <= 10) {
+    nivel = "Ferro";
+  } else if (vitorias >= 10 && vitorias <= 20) {
+    nivel = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+
+  return { saldo, nivel };
+}
+
+// Exemplo de uso
+const vitorias = 70;
+const derrotas = 10;
+const { saldo, nivel } = calcularRank(vitorias, derrotas);
+console.log(`O Madmax tem um saldo de ${saldo} está no nível ${nivel}`);
